e08c594576e581861c0a7e584d8d48583770af81
Jachmann <raoul.jachmann@capgemini.com>
2019-09-17 13:49:56 +0200

    Turned address into an entity and added an ID

--- src/main/java/com/capgemini/molveno/reserveringssysteem/model/Address.java
+++ src/main/java/com/capgemini/molveno/reserveringssysteem/model/Address.java
@@ -1,6 +1,16 @@
 package com.capgemini.molveno.reserveringssysteem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String streetName;
     private int houseNumber;
