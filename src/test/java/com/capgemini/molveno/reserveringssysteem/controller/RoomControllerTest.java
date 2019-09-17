f457f02997e6f5b915d884adce5415c4a79aa492
Kalloe <jamie.kalloe@capgemini.com>
2019-09-17 15:36:44 +0200

    Add the RoomController test

---
+++ src/test/java/com/capgemini/molveno/reserveringssysteem/controller/RoomControllerTest.java
@@ -0,0 +1,56 @@
+package com.capgemini.molveno.reserveringssysteem.controller;
+
+import com.capgemini.molveno.reserveringssysteem.model.BedType;
+import com.capgemini.molveno.reserveringssysteem.model.Room;
+import com.capgemini.molveno.reserveringssysteem.model.RoomType;
+import com.capgemini.molveno.reserveringssysteem.repository.RoomRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.when;
+
+public class RoomControllerTest {
+
+    @Mock
+    private RoomRepository mockedRoomRepository;
+
+    private RoomController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        this.controller = new RoomController(mockedRoomRepository);
+
+        when(mockedRoomRepository.findAll()).thenReturn(new ArrayList<>());
+        when(mockedRoomRepository.findById(1l)).thenReturn(Optional.of(new Room(1l, RoomType.SINGLE,1, 1, new BedType[]{}, false, 1)));
+    }
+
+    @Test
+    public void getAllRooms_void_returnEmptyArrayList() {
+        List<Room> expectedResult = new ArrayList<>();
+
+        List<Room> actualResult = this.controller.getAllRooms();
+
+        assertThat(actualResult, equalTo(expectedResult));
+    }
+
+    @Test
+    public void getRoom_id1_returnsRoom() {
+        Room expectedRoom = new Room(1l, RoomType.SINGLE,1, 1, new BedType[]{}, false, 1);
+
+        Room actualRoom = this.controller.getRoom(1l);
+
+        boolean equals = actualRoom.equals(expectedRoom);
+
+        assertThat(actualRoom, equalTo(expectedRoom));
+    }
+}
