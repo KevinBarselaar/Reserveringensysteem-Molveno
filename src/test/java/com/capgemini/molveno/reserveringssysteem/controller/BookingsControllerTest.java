f27593e13f83f7f9438a8de873806fa607d1b2ca
Jachmann <raoul.jachmann@capgemini.com>
2019-09-18 14:36:39 +0200

    Begin gemaakt aan test voor bookingscontroller

---
+++ src/test/java/com/capgemini/molveno/reserveringssysteem/controller/BookingsControllerTest.java
@@ -0,0 +1,33 @@
+package com.capgemini.molveno.reserveringssysteem.controller;
+
+import com.capgemini.molveno.reserveringssysteem.model.Booking;
+import com.capgemini.molveno.reserveringssysteem.model.Room;
+import com.capgemini.molveno.reserveringssysteem.repository.BookingRepository;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.mockito.Mockito.when;
+
+public class BookingsControllerTest {
+
+    @Mock   //'nep' repository waarmee je de repository kan na simuleren
+    private BookingRepository mockedBookingRepository;
+
+    private BookingsController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this); //Mockito maakt alle mocks aan van deze klasse
+
+        this.controller = new BookingsController(mockedBookingRepository);
+
+        List<Room> mockedRooms = Arrays.asList(new Room(), new Room());
+
+        when(mockedBookingRepository.findAll()).thenReturn(new ArrayList<>()); //gedrag specificeren
+        when(mockedBookingRepository.findById(1l)).thenReturn(Optional.of(new Booking(mockedRooms, "I'd like spider webs in my room", LocalDateTime.of(1993, 1,1,1,1), LocalDateTime.of(1995,1,1,1,1)))); //De repo wordt niet echt gebruikt, maar je vertelt hoe het zich moet gedragen
+    }
+}
