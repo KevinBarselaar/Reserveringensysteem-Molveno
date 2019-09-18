c4385493f2a7cd52f69143f18da898acbbd78edf
Said <andrew.said@capgemini.com>
2019-09-18 15:24:27 +0200

    Added comments/javadoc

--- src/main/java/com/capgemini/molveno/reserveringssysteem/io/RoomExcelDeserializer.java
+++ src/main/java/com/capgemini/molveno/reserveringssysteem/io/RoomExcelDeserializer.java
@@ -20,6 +20,11 @@ import java.util.regex.Pattern;
 @Component
 public class RoomExcelDeserializer {
 
+    /**
+     * Extracts all the room data from an excel sheet
+     * @param excelFile Excel file containing room data
+     * @return List of Room objects
+     */
     public List<Room> deserialize(File excelFile) {
         List<Room> rooms = new ArrayList<>();
         long roomId = 1;
@@ -36,6 +41,7 @@ public class RoomExcelDeserializer {
                 while (rowIterator.hasNext()) {
                     Row row = rowIterator.next();
 
+                    //Get room data from row
                     if (row.cellIterator().hasNext()) {
                         RoomType roomType = RoomType.from(row.getCell(0).toString());
                         String adultCapacity = row.getCell(1).toString();
@@ -64,24 +70,30 @@ public class RoomExcelDeserializer {
         return rooms;
     }
 
+    /**
+     * Gets all the different beds found in a room
+     * @param bedTypes Raw string containing number and types of bed
+     * @return Array containing the types of bed found in a room
+     */
     public BedType[] getBedTypesFromString(String bedTypes) {
         if (bedTypes == null || bedTypes.isEmpty()) {
             return null;
         }
 
         List<BedType> types = new ArrayList<>();
+        //Remove unnecessary data so that only the number of beds and types remain
         bedTypes = bedTypes.toUpperCase().replace("BEDS", "").replace("BED", "")
                 .replace("X", "").replace(",", "");
         String[] bedOptions = bedTypes.split(" ");
         bedOptions = deleteEmpty(bedOptions);
 
         for (int i = 0; i < bedOptions.length; i++) {
-            if (!bedOptions[i].equals("")) {
                 BedType bedType;
 
                 Matcher numberInStringRegex = Pattern.compile("\\d[0-9]{0,}").matcher(bedOptions[i]);
                 if (numberInStringRegex.find()) { //The option contains a number
                     int amountOfBeds = Integer.parseInt(numberInStringRegex.group(0));
+                    //Type of bed found in next value in array
                     bedType = getType(bedOptions[i+1]);
 
                     for (int bedIndex = 0; bedIndex < amountOfBeds; bedIndex++) {
@@ -89,16 +101,21 @@ public class RoomExcelDeserializer {
                     }
 
                     i++;
-                } else {
+                } else { //Option doesn't contain a number, which means a single bed of the type
                     types.add(getType(bedOptions[i]));
                 }
             }
-        }
         return types.toArray(new BedType[0]);
     }
 
+    /**
+     * Gets the BedType contained in a String
+     * @param bedType String containing info about BedType
+     * @return BedType found in String
+     */
     public BedType getType(String bedType) {
         String[] split = bedType.replaceAll("[0-9]", "").split("");
+        //Due to BedType naming we only have to check the first letter of the string to find the type of bed
         switch (split[0].toLowerCase()) {
             case "s":
                 return BedType.SINGLE;
@@ -112,8 +129,14 @@ public class RoomExcelDeserializer {
 
     }
 
+    /**
+     * Removes empty entries in an array
+     * @param array Array containing strings
+     * @return Array that was given, with the empty values removed
+     */
     public String[] deleteEmpty(String[] array) {
         ArrayList<String> result = new ArrayList<String>();
+        //Adds only the strings that are not empty into the ArrayList
         for(String string : array) {
             if(!string.equals("")) {
                 result.add(string);
