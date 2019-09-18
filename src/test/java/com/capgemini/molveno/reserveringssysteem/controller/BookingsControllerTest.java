1c8d03bf503716b58640bcbaefeae9c33412b00c
Jachmann <raoul.jachmann@capgemini.com>
2019-09-18 15:15:52 +0200

    kleine toevoeging aan test bookingscontroller

--- src/test/java/com/capgemini/molveno/reserveringssysteem/controller/BookingsControllerTest.java
+++ src/test/java/com/capgemini/molveno/reserveringssysteem/controller/BookingsControllerTest.java
@@ -4,12 +4,14 @@ import com.capgemini.molveno.reserveringssysteem.model.Booking;
 import com.capgemini.molveno.reserveringssysteem.model.Room;
 import com.capgemini.molveno.reserveringssysteem.repository.BookingRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
 import java.time.LocalDateTime;
 import java.util.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.when;
 
 public class BookingsControllerTest {
@@ -28,6 +30,15 @@ public class BookingsControllerTest {
         List<Room> mockedRooms = Arrays.asList(new Room(), new Room());
 
         when(mockedBookingRepository.findAll()).thenReturn(new ArrayList<>()); //gedrag specificeren
-        when(mockedBookingRepository.findById(1l)).thenReturn(Optional.of(new Booking(mockedRooms, "I'd like spider webs in my room", LocalDateTime.of(1993, 1,1,1,1), LocalDateTime.of(1995,1,1,1,1)))); //De repo wordt niet echt gebruikt, maar je vertelt hoe het zich moet gedragen
+        when(mockedBookingRepository.findById(1l)).thenReturn(Optional.of(new Booking(mockedRooms, "I'd like spider webs in my rooms", LocalDateTime.of(1993, 1,1,1,1), LocalDateTime.of(1995,1,1,1,1)))); //De repo wordt niet echt gebruikt, maar je vertelt hoe het zich moet gedragen
+    }
+
+    @Test
+    public void getAllBookings_void_returnEmptyArrayList() {
+        List<Booking> expectedResult = new ArrayList<>();
+
+        List<Booking> actualResult = this.controller.getAllBookings();
+
+        assertThat(actualResult, equalTo(expectedResult));
     }
 }
