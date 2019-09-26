676c419bb933e9aa455cf147018dc5aa3c6952f6
Kalloe <jamie.kalloe@capgemini.com>
2019-09-26 10:22:22 +0200

    Use Jquery DataTables to show the data

--- src/main/resources/static/js/bookings.js
+++ src/main/resources/static/js/bookings.js
@@ -1,10 +1,8 @@
-getBookingsOverview = function() {
+function getBookingsOverview() {
     $.get("/api/bookings/overview", function(bookingData) {
-        for(var index = 0; index < bookingData.length; index++) {
-            var booking = bookingData[index];
 
-            $("#bookings").append('<tr><th scope="row">' + booking.id + '</th><td>' + booking.guest.lastName + '</td><td>' + booking.startBooking +'</td><td>' + booking.endBooking + '</td></tr>');
-        }
+            $('#table').DataTable().clear();
+            $('#table').DataTable().rows.add(bookingData);
+            $('#table').DataTable().columns.adjust().draw();
     });
-
 }
