2c4035eaa0b4c1eee1a99db7c9dbf5db80b797f2
Jachmann <raoul.jachmann@capgemini.com>
2019-09-26 15:20:52 +0200

    for each loop

--- src/main/resources/static/js/booking-details.js
+++ src/main/resources/static/js/booking-details.js
@@ -3,7 +3,6 @@ function getBookingDetails() {
 //TODO get doen naar de backend voor specifieke booking (1)
     $.get("/api/bookings/1", function(data, status) {
         var booking = data;
-        console.log(booking);
 
         //retrieve personal data from back-end
         $("#full-name").text(booking.guest.firstName + " " + booking.guest.lastName);
@@ -21,14 +20,13 @@ function getBookingDetails() {
 
         //retrieve booking information from back-end
         $("#booking-period").text(booking.startBooking + " - " + booking.endBooking);
-        $("#extra-items").text(booking.extraAccommodation);
+        $("#extra-items").text(booking.extraItems);
 
         //retrieve booked room information from back-end
 
         var rooms = booking.rooms;
         var roomsContainer = $("#room-rows");
-        for(var index = 0; index < rooms.length; index++) {
-            var room = rooms[index];
+        rooms.forEach(function(room) {
             $("#room-table").append(createRoomTableHeader("Room", room.id));
             roomsContainer.append(createRoomTableRow("Room type", room.type));
             roomsContainer.append(createRoomTableRow("Adult capacity", room.adultCapacity));
@@ -37,8 +35,7 @@ function getBookingDetails() {
             roomsContainer.append(createRoomTableRow("Floor", room.floor));
             roomsContainer.append(createRoomTableRow("Disabled friendly", room.disabledFriendly));
             roomsContainer.append(createRoomTableRow("Price", room.price));
-
-        }
+        });
     });
 }
 
